709b6dc8dd34db5bc138bf8f732f4eb4d7a736b8
GUSTAVO JANUARIO BLAZECHI <g.blazechi@labcetec.net>
2025-06-27 19:08:34 -0300

    Terceiro commit: Push da main

--- src/main/java/com/mycompany/syntrex/Syntrex.java
+++ src/main/java/com/mycompany/syntrex/Syntrex.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.syntrex_telas;
+package com.mycompany.syntrex;
 
 /**
  *
  * @author g.blazechi
  */
-public class Syntrex_Telas {
+public class Syntrex {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
