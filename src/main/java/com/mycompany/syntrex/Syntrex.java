bd08a46655f6991536064c135d8b8e93b2b33d42
HERBERTE OLIVEIRA HUPP <h.hupp@labcetec.net>
2025-06-26 21:40:23 -0300

    versao atual 1.0

--- src/main/java/com/mycompany/syntrex/Syntrex.java
+++ src/main/java/com/mycompany/syntrex/Syntrex.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.syntrex;
+package com.mycompany.sintrex_ofc;
 
 /**
  *
- * @author a.s.oliveira
+ * @author h.hupp
  */
-public class Syntrex {
+public class Sintrex_ofc {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
